da6a7d009af55d652d37d8a91d7608c0093d26f4
devCJH <http-@naver.com>
2022-11-28 20:48:06 +0900

    ArticleDao, Article(dto) 도입

--- src/main/java/com/KoreaIT/java/am/service/ArticleService.java
+++ src/main/java/com/KoreaIT/java/am/service/ArticleService.java
@@ -2,44 +2,33 @@ package com.KoreaIT.java.am.service;
 
 import java.sql.Connection;
 import java.util.List;
-import java.util.Map;
 
-import com.KoreaIT.java.am.util.DBUtil;
-import com.KoreaIT.java.am.util.SecSql;
+import com.KoreaIT.java.am.dao.ArticleDao;
+import com.KoreaIT.java.am.dto.Article;
 
 public class ArticleService {
 
-	private Connection conn;
 	private int itemsInAPage;
+	private ArticleDao articleDao;
 	
 	public ArticleService(Connection conn) {
 		this.itemsInAPage = 10;
-		this.conn = conn;
+		this.articleDao = new ArticleDao(conn);
 	}
 
 	public int getListTotalPage() {
 
-		SecSql sql = SecSql.from("SELECT COUNT(id)");
-		sql.append("FROM article");
-		
-		int totalCount = DBUtil.selectRowIntValue(conn, sql);
+		int totalCount = articleDao.getListTotalCount();
 		int totalPage = (int)Math.ceil((double)totalCount / itemsInAPage);
 		
 		return totalPage;
 	}
 
-	public List<Map<String, Object>> getArticleRows(int page) {
+	public List<Article> getArticleRows(int page) {
 		
 		int limitFrom = (page - 1) * itemsInAPage;
 		
-		SecSql sql = SecSql.from("SELECT A.*, M.name AS writerName");
-		sql.append("FROM article AS A");
-		sql.append("INNER JOIN `member` AS M");
-		sql.append("ON A.memberId = M.id");
-		sql.append("ORDER BY A.id DESC");
-		sql.append("LIMIT ?, ?", limitFrom, itemsInAPage);
-		
-		return DBUtil.selectRows(conn, sql);
+		return articleDao.getArticles(limitFrom, itemsInAPage);
 	}
 	
 }
--- src/main/webapp/jsp/article/list.jsp
+++ src/main/webapp/jsp/article/list.jsp
@@ -1,10 +1,11 @@
 <%@ page import="java.util.List" %>
 <%@ page import="java.util.Map" %>
+<%@ page import="com.KoreaIT.java.am.dto.Article" %>
 <%@ page import="java.time.LocalDateTime" %>
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <%
-List<Map<String, Object>> articleRows = (List<Map<String, Object>>)request.getAttribute("articleRows");
+List<Article> articles = (List<Article>)request.getAttribute("articles");
 int cPage = (int)request.getAttribute("page");
 int totalPage = (int)request.getAttribute("totalPage");
 %>
@@ -40,12 +41,12 @@ int totalPage = (int)request.getAttribute("totalPage");
 			<th>작성자</th>
 		</tr>
 	
-	<% for(Map<String, Object> articleRow : articleRows){ %>
+	<% for(Article article : articles){ %>
 		<tr>
-			<td><%= (int)articleRow.get("id") %></td>
-			<td><%= (LocalDateTime)articleRow.get("regDate") %></td>
-			<td><a href="detail?id=<%= (int)articleRow.get("id") %>"><%= (String)articleRow.get("title") %></a></td>
-			<td><%= (String)articleRow.get("writerName") %></td>
+			<td><%= (int)article.id %></td>
+			<td><%= (LocalDateTime)article.regDate %></td>
+			<td><a href="detail?id=<%= (int)article.id %>"><%= (String)article.title %></a></td>
+			<td><%= (String)article.writerName %></td>
 		</tr>
 	<% } %>
 	</table>
